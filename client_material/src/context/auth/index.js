f032d6f2055802096b7aece04f9bfca756ef474e
Mustafa Akbaş <makbash21@gmail.com>
2021-10-09 20:08:54 +0300

    old client_material files deleted

--- client_material/src/context/auth/index.js
+++
@@ -1,4 +0,0 @@
-import { loginUser, logout, checkAuthenticated, load_user, signup } from './actions';
-import { AuthProvider, useAuthDispatch, useAuthState } from './context';
-
-export { AuthProvider, useAuthState, useAuthDispatch, loginUser, logout, load_user, checkAuthenticated, signup };
